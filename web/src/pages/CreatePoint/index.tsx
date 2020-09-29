a1eb914a83cae685ccbbaac8428a3b15ad97d806
MarciaTzannis <marciatzannis@gmail.com>
2020-09-29 12:54:45 -0300

    Integração Mapa Leaflet

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
 
 import "./styles.css";
 
@@ -48,6 +49,14 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione um endereço no mapa</span>
           </legend>
+
+          <Map center={[-23.5792435, -46.6415725]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-23.5792435, -46.6415725]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
